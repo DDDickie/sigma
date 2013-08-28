558657eb08ce41d572b6d491feb3455156176372
acer <acer@ubuntu.ubuntu-domain>
2013-08-28 22:39:29 +0800

    added static templates

--- static/js/log_in.js
+++ static/js/log_in.js
@@ -24,7 +24,27 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-	$(".log_in").click(function(){
+	$(".login_menu").click(function(){
 		openme();
+	});
+});
+
+$(document).ready(function(){
+	$(".login_button").click(function(){
+		var username_length=$(".login_username").val().length;
+		var password_length=$(".login_password").val().length;
+		if(username_length==0&&password_length>0)
+			alert("请输入用户名");
+		else if(username_length>0&&password_length==0)
+				alert("请输入密码");
+			else if(username_length==0&&password_length==0)
+					alert("请输入用户名和密码");
+				else logo_in();
+	});
+});
+
+$(document).ready(function(){
+	$(".login_exit_button").click(function(){
+		closeme();
 	});
 });
